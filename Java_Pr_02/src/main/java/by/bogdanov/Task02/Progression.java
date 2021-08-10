010107dfa390e73fcb79a055accc3c955c60f778
Anton Bogdanov <folland1@inbox.ru>
2021-08-10 14:12:21 +0300

    new

---
+++ Java_Pr_02/src/main/java/by/bogdanov/Task02/Progression.java
@@ -0,0 +1,26 @@
+package by.bogdanov.task02;
+
+/**
+ * for current value define arithmetic progression
+ */
+
+public class Progression {
+
+    public static boolean arithmeticProgress(String number){
+        char[] c = number.toCharArray();
+        int a = c[1]-c[0];
+        int b = 0;
+        for(int i=1; i<c.length; i++) {
+           if(c.length<=2){
+               System.out.println("Number must contain more than 2 digits !");
+               Viewer.log.error("Number must contain more than 2 digits !");
+               break;
+           }
+           if(c[i]-c[i-1]==a){
+           b++;
+           }
+        }
+        return b == c.length - 1;
+
+    }
+}
